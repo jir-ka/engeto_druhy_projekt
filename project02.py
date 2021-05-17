7537987afd5de8d6ff21b1d12c7d11332606a709
jir-ka <jklikar@seznam.cz>
2021-05-17 11:23:14 +0200

    hra

--- project02.py
+++ project02.py
@@ -1,1 +1,8 @@
-nazdar
+print("Hi there!")
+print(80 * '-')
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print(80 * '-')
+
+
+print("Enter a number:")
